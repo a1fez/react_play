0fc0b34ede8df2e751d4e8cd56dff74f65c94ede
Кирилл <a1fezfora1fez@gmail.com>
2026-02-15 04:45:30 +0200

    updated README

---
+++ README.md
@@ -0,0 +1,7 @@
+## Just React playground to learn React
+
+# Notes 
+
+- created react test project, lerned how to use useSteate, what means JSX and figured out with "setCount" function
+
+-created some own UI. deleted original React UI, added own div and button without functions
---
+++ react-playground/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+    <h1>My React App</h1>
+
+    <button>Hey, click me</button>
+
+    <div> Clicked times </div>
+
+    </div>
+  )
+}
+
+export default App
